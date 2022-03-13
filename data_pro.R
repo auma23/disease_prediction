c91a1e245bc8e141896e1cbeca67b0753e898770
auma23 <dianaauma75@gmail.com>
2022-03-13 23:34:01 +0300

    final work on desease prediction

--- data_pro.R
+++ data_pro.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+library(randomForest)
 view(Training)
 head(Training)
 dim(Training)
@@ -9,3 +11,25 @@ train_set<-Training[!complete.cases(Training),]
 train_set<-train_set[,-length(train_set)]
 dim(train_set)
 unique(train_set$prognosis)
+test_set<-Testing[,-length(Testing)]
+rf<-randomForest(prognosis~.,data=train_set)
+train_set$prognosis<-factor(train_set$prognosis)
+class(train_set$prognosis)
+glimpse(test_set)
+test_set = subset(test_set, select = -c(`spotting_ urination`) )
+names(test_set)
+rename_with(test_set, "spotting_ urination" = spotting_urination)
+test_set <- test_set %>%
+  rename(`spotting_urination` = `spotting_ urination` )           
+rf<-randomForest(prognosis~.,data=train_set)
+train_set <- train_set %>%
+  rename(`foul_smell_of_urine` = `foul_smell_of urine` )
+train_set <- train_set %>%
+  rename( `toxic_look_typhos` = `toxic_look_(typhos)` )
+train_set <- train_set %>%
+  rename( `dischromic_patches` = `dischromic _patches` )
+importance(rf)
+dim(train_set)
+train_set<- train_set %>%
+  rename(`spotting_urination` = `spotting_ urination`)
+names(train_set)
